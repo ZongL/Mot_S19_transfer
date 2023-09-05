82b900169bcc474356c560e707ded984def8dab4
ZongL <zongliang88888@163.com>
2023-09-05 10:33:27 +0800

    Update CMAC

--- GenS19FromMot.bat
+++ GenS19FromMot.bat
@@ -1,34 +1,40 @@
- 
-C:\srecord\bin\srec_cat XXXXXXX.mot -exclude 0x343A8000 0x343A8138 -o zTemp00.srec
+C:\srecord\bin\srec_cat *.mot -exclude 0x20000004 0x200001AC -o zTemp00.srec
 
-C:\srecord\bin\srec_cat zTemp00.srec -fill 0x1A 0x342FFE00 0x342FFE01 -fill 0x2B 0x342FFE01 0x342FFE02 -fill 0x3C 0x342FFE02 0x342FFE03 -fill 0x4D 0x342FFE03 0x342FFE04 --output zTemp11.srec
+C:\srecord\bin\srec_cat zTemp00.srec -fill 0x1A 0x342FFE00 0x342FFE01 -fill 0x2B 0x342FFE01 0x342FFE02 -fill 0x3C 0x342FFE02 0x342FFE03 -fill 0x4D 0x342FFE03 0x342FFE04 --output mergedCAL.srec
 
-C:\srecord\bin\srec_cat zTemp11.srec Didi_CAL_0824.s19 -o mergedCAL.srec
+::C:\srecord\bin\srec_cat zTemp11.srec Didi_CAL_0824.s19 -o mergedCAL.srec
 
-C:\srecord\bin\srec_cat mergedCAL.srec -fill 0x1A 0x3477FE00 0x3477FE01 -fill 0x2B 0x3477FE01 0x3477FE02 -fill 0x3C 0x3477FE02 0x3477FE03 -fill 0x4D 0x3477FE03 0x3477FE04  --output mergedCALFilled.srec
+C:\srecord\bin\srec_cat mergedCAL.srec -fill 0x1A 0x347FFE00 0x347FFE01 -fill 0x2B 0x347FFE01 0x347FFE02 -fill 0x3C 0x347FFE02 0x347FFE03 -fill 0x4D 0x347FFE03 0x347FFE04  --output mergedCALFilled.srec
 
 
-C:\srecord\bin\srec_cat bootloader.img -binary -offset 0x33000000 -o bootloaderoffset.srec
+C:\srecord\bin\srec_cat *.img -binary -offset 0x33000000 -o bootloaderoffset.srec
 C:\srecord\bin\srec_cat mergedCALFilled.srec bootloaderoffset.srec -o mergedCAL_UBoot.srec
 C:\srecord\bin\srec_cat mergedCAL_UBoot.srec -fill 0x1A 0x333FFE00 0x333FFE01 -fill 0x2B 0x333FFE01 0x333FFE02 -fill 0x3C 0x333FFE02 0x333FFE03 -fill 0x4D 0x333FFE03 0x333FFE04  --output mergedCAL_UBootFilled.srec
 
 
-C:\srecord\bin\srec_cat 20221214_didi_E5_gPTP_fw_config.bin -binary -offset 0x33500000 -o Switchoffset.srec
+C:\srecord\bin\srec_cat *.bin -binary -offset 0x33500000 -o Switchoffset.srec
 C:\srecord\bin\srec_cat mergedCAL_UBootFilled.srec Switchoffset.srec -o mergedCAL_UBoot_Switch.srec
 C:\srecord\bin\srec_cat mergedCAL_UBoot_Switch.srec -fill 0x1A 0x336FFE00 0x336FFE01 -fill 0x2B 0x336FFE01 0x336FFE02 -fill 0x3C 0x336FFE02 0x336FFE03 -fill 0x4D 0x336FFE03 0x336FFE04  --output mergedCAL_UBoot_SwitchFilled.srec
 
-C:\srecord\bin\srec_cat mergedCAL_UBoot_SwitchFilled.srec -fill 0x00 -within mergedCAL_UBoot_SwitchFilled.srec -range-pad 256 --output XXXXXXX_out.s19
+C:\srecord\bin\srec_cat mergedCAL_UBoot_SwitchFilled.srec -fill 0x00 -within mergedCAL_UBoot_SwitchFilled.srec -range-pad 256 --output NoCMAC_mcu.s19 -disable=data-count
 
-del zTemp00.srec
-del zTemp11.srec
-del mergedCAL.srec
-del mergedCALFilled.srec
-del bootloaderoffset.srec
-del mergedCAL_UBoot.srec
-del mergedCAL_UBootFilled.srec
-del Switchoffset.srec
-del mergedCAL_UBoot_Switch.srec
-del mergedCAL_UBoot_SwitchFilled.srec
+del *.srec
+ 
+echo "add CMAC to S19"
+ 
+::pause
 
+C:\srecord\bin\srec_cat NoCMAC_mcu.s19 -crop 0x34000000 0x342FFCFF -o data_extract.srec
 
-pause
+C:\srecord\bin\srec_cat data_extract.srec -fill 0xFF 0x34000000 0x342FFD00 -o data_extract_new.srec
+
+C:\srecord\bin\srec_cat data_extract_new.srec -offset -0x34000000 -o data.binn -Binary
+
+cmac_gen.exe
+
+C:\srecord\bin\srec_cat CMAC.binn -binary -offset 0x342FFD00 -o CMAC.binn.srec
+
+C:\srecord\bin\srec_cat NoCMAC_mcu.s19 CMAC.binn.srec -o mcu.img -disable=data-count
+del *.srec
+
+::pause
